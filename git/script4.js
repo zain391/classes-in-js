23dae256224273a16bf0a5957f88560158bb9cb7
zain391 <zainqamar391@gmail.com>
2022-01-28 23:57:47 +0500

    added the forth file
    added the forth file

---
+++ git/script4.js
@@ -0,0 +1,7 @@
+let selfCtx = self.ctx;
+let entity;    
+let intervalInstance;
+
+let STREAM_REQUEST_INTERVAL = 1000;
+let RPC_START_METHOD = "start_kvs_stream";
+let SERVER_API_CALL_RESPONSE = 'server-api-call-response';
